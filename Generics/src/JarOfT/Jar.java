b9c8f94d2746acccea8a3d442b82d862bd1c739c
baldzhiyski <hristo.bld032@gmail.com>
2023-10-07 13:18:53 +0300

    Adding results Generics

---
+++ Generics/src/JarOfT/Jar.java
@@ -0,0 +1,18 @@
+package JarOfT;
+
+import java.util.ArrayDeque;
+
+public class Jar<T> {
+    private ArrayDeque<T> stack;
+
+    public Jar(){
+        this.stack = new ArrayDeque<>();
+    }
+
+    public void add(T element){
+        stack.push(element);
+    }
+    public T remove(){
+       return stack.pop();
+    }
+}
